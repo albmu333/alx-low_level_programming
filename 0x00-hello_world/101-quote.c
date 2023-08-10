c5a9f2f743c6b79775ccf0509a2e96b991eb4fed
albmu333 <mo5@example.com>
2023-08-10 10:54:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <unisstd.h>
 /**
- * main - A c program that prints a line to the standard error
- * describtion  
+ * main - 101-quote.c
+ * description - Write a C program that prints exactly
+ *
+ * Return: alaways (1) (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\"  Dora korpar, 2015-10-19\n", stdout);
+	reutrn (1);
+}
