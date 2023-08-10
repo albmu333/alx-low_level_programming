5e998ce04cf1127ccafe39e728d1fd6ef550d3b8
albmu333 <mo5@example.com>
2023-08-10 08:31:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-##include <stdio.h>
+#include <stdio.h>
 /**
- *main -entry poit
- * Return: always 0 (Success)
- */
+*main - entry poit
+*return : always 0 (success)
+*/
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle")
-	return (0);
-#include <stdio.h>}
+	puts("\"Proramming is like building a multilingual puzzle");
+	reyurn (0);
+}
