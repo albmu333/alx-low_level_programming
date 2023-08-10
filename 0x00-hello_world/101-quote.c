9da483982c03be6ea39bb6e604b74e880c106022
albmu333 <mo5@example.com>
2023-08-10 11:12:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unisstd.h>
+
 /**
  * main - 101-quote.c
  * description - Write a C program that prints exactly
  *
- * Return: alaways (1) (success)
+ * Return: alaways
  */
 int main(void)
 {
-	fput("and that piece of art is useful\"  Dora korpar, 2015-10-19\n", stdout);
-	reutrn (1);
+	fput("and that piece of art is useful\" Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }
