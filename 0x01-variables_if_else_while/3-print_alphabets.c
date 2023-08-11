87bd6e1119f25cab84b79987cef80cf906ccd7e4
albmu333 <mo5@example.com>
2023-08-11 16:41:18 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,12 +3,12 @@
 /**
  * main - Prints the alphabet.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char alp[52] ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTYVWXYZ"
-	int i:
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
 	for (i = 0; i < 52; i++)
 	{
