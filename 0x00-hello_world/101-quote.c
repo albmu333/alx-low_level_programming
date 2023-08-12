8c93391f05c626fb09f877476597d0c30ea2eafc
albmu333 <mo5@example.com>
2023-08-11 17:10:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <unisstd.h>
+#include <unistd.h>
 
 /**
- * main - 101-quote.c
- * description - Write a C program that prints exactly
- *
- * Return: alaways
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\" Dora korpar, 2015-10-19\n", 59);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
